5956b73ae5568f8b8ec91e79516bc3174acf45ef
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-23 08:03:25 -0700

    feat(tmc-cli): add formula homepage

--- Formula/tmc-cli.rb
+++ Formula/tmc-cli.rb
@@ -1,6 +1,6 @@
 class TmcCli < Formula
   desc "Command line client for tmc"
-  homepage ""
+  homepage "https://tmc.mooc.fi/"
   url "https://github.com/testmycode/tmc-cli/archive/refs/tags/0.9.3.tar.gz"
   sha256 "f9b7d4e6d92921fe618598b3f64615703ab9e6bc980cba9c7a812fc39d1b5992"
   license "MIT"
